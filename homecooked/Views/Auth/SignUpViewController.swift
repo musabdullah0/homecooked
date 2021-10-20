cae25f60e30b520a6ee92bf744a66dbe7043a6f3
Musab Abdullah <musababdullah6231@gmail.com>
2021-10-20 16:03:11 -0500

    setup authentication kind of

---
+++ homecooked/Views/Auth/SignUpViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SignUpViewController.swift
+//  homecooked
+//
+//  Created by Musab Abdullah on 10/20/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickedLogin(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "login") as! LoginViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    @IBAction func clickedSignup(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let strongSelf = self else { return }
+                if let error = error {
+                    print("sign up error: \(error.localizedDescription)")
+                } else {
+                    print("sign up success: \(String(describing: authResult))")
+                    let vc = strongSelf.storyboard?.instantiateViewController(withIdentifier: "tabbar") as! UIViewController
+                    vc.modalPresentationStyle = .fullScreen
+                    strongSelf.present(vc, animated: true)
+                }
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
